6bdaedfbe4972952f2dfbc1bc8e9d602377b326b
Alberto Hitiro Tsugawa <hitiro@live.com>
2022-12-24 13:43:35 -0300

    feat: concluindo e deletando uma tarefa

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -8,7 +8,9 @@ import {
   onSnapshot,
   query,
   orderBy,
-  where
+  where,
+  doc,
+  deleteDoc
 } from 'firebase/firestore'
 
 export default function Admin() {
@@ -38,9 +40,9 @@ export default function Admin() {
             })
           })
 
-          console.log(lista);
 
           setTarefas(lista);
+          console.log(tarefas)
         })
 
       }
@@ -75,6 +77,11 @@ export default function Admin() {
     await signOut(auth);
   }
 
+  async function deleteTarefa(id) {
+    const docRef = doc(db, "tarefas", id);
+    await deleteDoc(docRef);
+  }
+
   return (
     <div className="admin-container">
       <h1>Minhas Tarefas</h1>
@@ -89,14 +96,16 @@ export default function Admin() {
         <button className="btn-register" type="submit">Registrar tarefa</button>
       </form>
 
-      <article className="list">
-        <p>Estudar JavaScript e React hoje anoite.</p>
+      {tarefas.map((item) => (
+        <article key={item.id} className="list">
+          <p>{item.tarefa}</p>
 
-        <div>
-          <button>Editar</button>
-          <button className="btn-delete">Concluir</button>
-        </div>
-      </article>
+          <div>
+            <button>Editar</button>
+            <button onClick={() => deleteTarefa(item.id)} className="btn-delete">Concluir</button>
+          </div>
+        </article>
+      ))}
 
       <button className="btn-logout" onClick={handleLogout}>Sair</button>
     </div>
