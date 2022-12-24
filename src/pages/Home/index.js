43a61a692c9edc93ee5821bf4dd752c6d52695e9
Alberto Hitiro Tsugawa <hitiro@live.com>
2022-12-24 12:07:31 -0300

    ffeat: criando pagina de admin

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,16 +3,31 @@ import './home.css'
 
 import { Link } from 'react-router-dom'
 
+import { auth } from '../../firebaseConnection'
+import { signInWithEmailAndPassword } from 'firebase/auth';
+
+import { useNavigate } from 'react-router-dom';
+
 export default function Home() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  function handleLogin(e) {
+  const navigate = useNavigate();
+
+  async function handleLogin(e) {
     e.preventDefault();
 
     if (email !== '' && password !== '') {
 
-      alert("Teste");
+      await signInWithEmailAndPassword(auth, email, password)
+        .then(() => {
+          // Navegar para /admin
+          navigate('/admin', { replace: true });
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+
     } else {
       alert("Preencha todos os campos");
 
@@ -35,7 +50,6 @@ export default function Home() {
           onChange={(e) => setEmail(e.target.value)}
         />
         <input
-          autoComplete={false}
           type="password"
           placeholder="Digite a sua senha"
           value={password}
